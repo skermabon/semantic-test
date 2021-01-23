9ff9eac3ec5af9c4c125edcc0c82480ba7122281
Kermabon Stéphane <skermabon@talend.com>
2021-01-23 14:53:43 +0100

    Add discovery, validation

--- src/main/java/org/talend/hackathon/Main.java
+++ src/main/java/org/talend/hackathon/Main.java
@@ -7,6 +7,7 @@ import org.talend.dataquality.common.inference.Metadata;
 import org.talend.dataquality.semantic.api.CategoryRegistryManager;
 import org.talend.dataquality.semantic.api.SemanticProperties;
 import org.talend.dataquality.semantic.datamasking.ValueDataMasker;
+import org.talend.dataquality.semantic.recognizer.DefaultCategoryRecognizer;
 import org.talend.dataquality.semantic.snapshot.DeletableDictionarySnapshot;
 import org.talend.dataquality.semantic.statistics.SemanticAnalyzer;
 import org.talend.dataquality.semantic.statistics.SemanticType;
@@ -17,6 +18,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -41,30 +43,51 @@ public class Main {
 
 
             // Discovery - found the type of the column
-            List<String> values = asList("FR7630006000011234567890189", "FR0812739000402773652693J61", "FR7414508000501371141778U25");
+            List<String> values = asList(null, "FR7630006000011234567890189", "FR0812739000402773652693J61", "FR7414508000501371141778U25", "toto");
             ArrayList<String> column = new ArrayList();
             // column name
             column.add("Iban");
             // column values
             column.addAll(values);
             System.out.println("Column " + column);
+
+            // Found the semantic type of the column
             List<SemanticType> typeFrequencies =
                     semanticDiscovery(dictionarySnapshot, true, column);
-
             SemanticType semanticType = typeFrequencies.get(0);
-            System.out.println("Found semantic Type " + semanticType.getSuggestedCategory());
+            String semanticTypeName = semanticType.getSuggestedCategory();
+            System.out.println("Found semantic Type " + semanticTypeName);
 
-            // The semantic type is IBAN
-            ValueDataMasker masker = new ValueDataMasker(semanticType.getSuggestedCategory(), "string", dictionarySnapshot);
+            // Initiate the masker
+            ValueDataMasker masker = new ValueDataMasker(semanticTypeName, "string", dictionarySnapshot);
+
+            DefaultCategoryRecognizer categoryRecognizer = new DefaultCategoryRecognizer(dictionarySnapshot);
+            categoryRecognizer.prepare();
 
             ArrayList<String> results = new ArrayList<>(values.size());
             for (String value: values) {
-                System.out.println("Value to mask " + value);
-                String result = masker.maskValue(value);
-                results.add(result);
-                System.out.println("Masked value " + masker.maskValue(value));
+                if (value == null || value.isEmpty()) {
+                    System.out.println("Unmasked value " + value);
+                    results.add(value);
+                }
+                else {
+                    // Categories found for this value
+                    List<String> categories = Arrays.asList(categoryRecognizer.process(value));
+                    // if categories found contains category of the column
+                    if (categories.contains(semanticTypeName)) {
+                        System.out.println("Value to mask " + value);
+                        String result = masker.maskValue(value);
+                        results.add(result);
+                        System.out.println("Masked value " + masker.maskValue(value));
+                    } else {
+                        System.out.println("Unmasked value " + value);
+                        results.add(value);
+                    }
+                }
             }
+            categoryRecognizer.end();
 
+            System.out.println("Values " + values);
             System.out.println("Result " + results);
         }
         finally {
@@ -96,5 +119,29 @@ public class Main {
             return analyzer.getResult();
         }
     }
+
+//    private static List<Analyzers.Result> validation(DeletableDictionarySnapshot dictionarySnapshot, Boolean hasHeader,
+//                                             List<String> lines, DataTypeEnum[] dataTypes, String[] semanticTypeAsArray) {
+//        try (SemanticQualityAnalyzer semanticQualityAnalyzer =
+//                     new SemanticQualityAnalyzer(dictionarySnapshot, semanticTypeAsArray, true)) {
+//            return getResult(lines, hasHeader,
+//                    new ValueQualityAnalyzer(new DataTypeQualityAnalyzer(dataTypes), semanticQualityAnalyzer, true));
+//        }
+//    }
+//
+//    private static List<Analyzers.Result> getResult(List<String> lines, boolean hasHeader, Analyzer<?>... analyzers) {
+//        try (final Analyzer<Analyzers.Result> analyzer = Analyzers.with(analyzers)) {
+//            if (hasHeader)
+//                lines.remove(0);
+//            for (String line : lines) {
+//                analyzer.analyze(line.split(";"));
+//            }
+//            return analyzer.getResult();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e.getMessage(), e);
+//        }
+//    }
+
+
 
 }
